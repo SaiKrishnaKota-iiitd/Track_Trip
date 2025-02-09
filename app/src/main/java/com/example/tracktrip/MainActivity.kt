72c9b01013a93a40523b0803ad891344b89f7cd0
SaiKrishnaKota-iiitd <sai24078@iiitd.ac.in>
2025-02-10 03:47:23 +0530

    added logic for UI behaviour including progressbar, distance covered and distance left

--- app/src/main/java/com/example/tracktrip/MainActivity.kt
+++ app/src/main/java/com/example/tracktrip/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.tracktrip
 
 import android.graphics.fonts.Font
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -33,11 +34,13 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -45,7 +48,10 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tracktrip.ui.theme.TrackTripTheme
+import java.io.BufferedReader
 import java.io.File
+import java.io.FileReader
+import java.io.InputStreamReader
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,7 +60,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             TrackTripTheme {
                 Surface {
-                    StopList(stopList = mutableListOf("stop1","stop2","stop3","stop4","stop5"))
+//                    StopList(stopList = mutableListOf("stop1","stop2","stop3","stop4","stop5"))
+                    StopList()
                 }
             }
         }
@@ -78,15 +85,14 @@ fun GreetingPreview() {
 }
 
 
-
-
 @Preview( showBackground = false)
 @Composable
 fun TripAppPreview (
     color : Color = MaterialTheme.colorScheme.background
 ) {
     var tempList = mutableListOf("stop1","stop2","stop3","stop4","stop5")
-    StopList(stopList = tempList )
+//    StopList(stopList = tempList )
+    StopList()
 }
 
 @Composable
@@ -96,11 +102,23 @@ fun StopList(
         .fillMaxSize()
         .padding(top = 40.dp),
     color : Color = Color.Black,
-    stopList : MutableList<String>,
+//    stopList : MutableList<String>,
 ) {
+    val curcontext = LocalContext.current
+    var totalDistance = 0
+    var curprogress  =  remember { mutableStateOf(0) }
     var stopCount = remember { mutableStateOf(0) }
+    var stopList = remember { Readtext(context = curcontext) }
+    var isKM = remember { mutableStateOf(true) }
+    for (i in 1..stopList.size-1) totalDistance += stopList[i].distFromSource
     var nextStop : () -> Unit = {
-        if (stopCount.value < stopList.size ) stopCount.value++
+        if (stopCount.value < stopList.size ) {
+//            for (i in 0..stopCount.value) {
+                curprogress.value += stopList[stopCount.value].distFromSource
+//            }
+            Log.i("curprog","$curprogress")
+            stopCount.value++
+        }
         else stopCount.value
     }
     Column (
@@ -114,14 +132,14 @@ fun StopList(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(vertical = 10.dp, horizontal = 16.dp)
-                    .background(color = Color(0xff1d2024),shape = RoundedCornerShape(6))
+                    .background(color = Color(0xff1d2024), shape = RoundedCornerShape(6))
                     .weight(1f),
 //                    .align(alignment = Alignment.CenterVertically),
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center,
 
             ) {
-                itemsIndexed(stopList) { ind,stopName ->
+                itemsIndexed(stopList) { ind,curstop ->
 //                    Text(text = "stop $ind",
 //                        modifier  = Modifier
 //                            .padding(bottom = 16.dp),
@@ -131,7 +149,7 @@ fun StopList(
                         .width(300.dp)
                         .padding(top = 15.dp)
                     ) {
-                        Text("stop $ind",
+                        Text("${curstop.stopName}",
                             color = if (ind <= stopCount.value) Color(0xffdae2f9) else Color(0xffaac7ff),
                             textAlign = TextAlign.Center,
 //                            fontFamily = FontFamily(fonts = sa),
@@ -139,7 +157,10 @@ fun StopList(
                             fontWeight = FontWeight.ExtraBold,
                             modifier = Modifier
                                 .fillMaxWidth()
-                                .background(color = Color(0xff3e4759), shape = RoundedCornerShape(60))
+                                .background(
+                                    color = Color(0xff3e4759),
+                                    shape = RoundedCornerShape(60)
+                                )
                                 .padding(vertical = 10.dp)
 
                         )
@@ -157,25 +178,45 @@ fun StopList(
             ) {
                 for ( i in 0..stopList.size-1) {
                     Box(modifier = Modifier.background(color = Color(0xff284777))) {
-                        Text("stop $i",
+                        Text("${stopList[i].stopName}",
                             color = if (i <= stopCount.value) Color.Red else Color.Blue
                         )
                     }
                 }
             }
         }
-        LinearProgressIndicator(
-            progress = { (stopCount.value + 1).toFloat()/stopList.size },
+        Column (
             modifier = Modifier
-                .align(alignment = Alignment.CenterHorizontally),
-        )
+                .fillMaxWidth()
+                .background(color = Color(0xff1d2024), shape = RoundedCornerShape(12))
+                .padding(vertical = 20.dp, horizontal = 36.dp),
+//                    .align(alignment = Alignment.CenterVertically),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+        ) {
+            LinearProgressIndicator(
+                progress = { curprogress.value/totalDistance.toFloat() },
+                modifier = Modifier
+                    .align(alignment = Alignment.CenterHorizontally),
+            )
+            Spacer(modifier = Modifier.size(20.dp))
+            Text(
+                text = "Distance Covered : ${if (isKM.value) curprogress.value else curprogress.value*2}",
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.size(20.dp))
+            Text(
+                text = "Distance left : ${if(isKM.value) (totalDistance - curprogress.value) else (totalDistance - curprogress.value)*2}"
+            )
+        }
+
         Spacer(modifier = Modifier.size(20.dp))
         Row (
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.Center
         ) {
             Button(
-                onClick = { Log.i("onclick handler","to change units") },
+                onClick = { isKM.value = !isKM.value },
                 colors = ButtonDefaults.buttonColors(
                     containerColor = Color(0xffddbce0)
                 )
@@ -202,15 +243,50 @@ fun StopList(
     }
 }
 
+data class Stop (val stopName : String,val distFromSource : Int,val visaRequired : Boolean)
 
-
-fun ReadAndSetStops() {
-    val fileName  = "../res/stops.txt"
-    var stopList : List<String>? = File(fileName).useLines { it.toList() }
-    var stopDist : List<Float>? = null
-    if (stopList != null) {
-        for (i in stopList) {
-            Log.i("stop is :",i)
+fun ReadAndSetStops() : MutableList<Stop> {
+    val fileName  = "assets/stops.csv"
+    var stops = mutableListOf<Stop>()
+    var file = File(fileName)
+//    var newfile = Context.
+    var lines = file.readLines()
+    var tokens = listOf<String>()
+    lines.forEach {textlist ->
+        tokens = textlist.split(",")
+        if (tokens.size == 3) {
+            stops.add(
+                Stop(
+                    stopName = tokens[0],
+                    distFromSource = tokens[1].toInt(),
+                    visaRequired = if (tokens[2] == "yes") true else false
+                )
+            )
         }
     }
+
+    var inp =
+    return stops
+}
+
+
+fun Readtext (context: Context) : MutableList<Stop> {
+    var curfile = context.assets.open("stops.txt")
+    var curReader = BufferedReader(InputStreamReader(curfile))
+    var tokens = listOf<String>()
+    var stops = mutableListOf<Stop>()
+    Log.i("successful","file read")
+    curReader.readLines().forEach { textline ->
+        tokens = textline.split(",")
+        if (tokens.size == 3) {
+            stops.add(
+                Stop(
+                    stopName = tokens[0],
+                    distFromSource = tokens[1].toInt(),
+                    visaRequired = if (tokens[2] == "yes") true else false
+                )
+            )
+        }
+    }
+    return stops
 }
